c3ce71ce154fb5ca31ec0f0052c002b4f2c62496
Dck Token <dicktoken@gmail.com>
2016-10-12 18:42:52 +0200

    Made some basic shit

--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,7 @@
 import random
 
-board = ["X", "O", "O", "X", "X", "O", "X", "X", "O"]
+#board = ["X", "O", "O", "X", "X", "O", "X", "X", "O"] #for testing purpouses
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " ", ]
 
 def printBoard():
 	print('   |   |')
@@ -16,14 +17,45 @@ def printBoard():
 	print('   |   |')
 
 def chooseLetter():
-	print('Do you want to be X or Y?')
-	letter = str(input().upper())
-	if letter == "X" or letter == "Y":
-		print('[DEBUG] You did good')
-		return
+	print('Do you want to be X or O?')
+	userInput = input().upper()
+	if userInput == "X":
+		letter = ["X", "O"]
+		global letter
+	if userInput == "O":
+		letter = ["O", "X"]
+		global letter
 	else:
 		print('Invalid input')
 		chooseLetter()
 
-printBoard()
-chooseLetter()
+def whoGoesFirst():
+	#randomly chooses who goes first (with a 0 for user and 1 for ia)
+	print('Randomly choosing who goes first...')
+	firstMove = random.randint(0, 1)
+	if firstMove == 0:
+		print('You go first!')
+	if firstMove == 1:
+		print('IA goes first')
+	currentMove = firstMove
+	global currentMove
+
+def placeMove(move):
+	board[move] = letter[currentMove]
+
+def movement():
+	print('What is your movement?')
+	move = int(input())
+	placeMove(move)
+
+def game():
+	printBoard()
+	chooseLetter()
+	whoGoesFirst()
+	if currentMove == 0:
+		movement()
+		printBoard()
+	else:
+		print('WIP')
+
+game()
